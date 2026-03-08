671df7cf2d6b382ab71a48b62294619025c6e5d2
alinabekbala <alina.bekbala@gmail.com>
2026-03-08 14:09:09 +0500

    fix dependencies for streamlit cloud

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 import yaml
 from pathlib import Path
 from datetime import datetime
